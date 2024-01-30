8112c12c33c71698584c083b2fa9af3bc498b2fb
McKayla Ford <117466529+McK-Ford@users.noreply.github.com>
2024-01-30 11:06:18 -0500

    Troubleshooting a R issue in ttseqfinal.sh

--- Bash scripting/TTseq/fixbws.sh
+++ Bash scripting/TTseq/fixbws.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --mem=128G
-#SBATCH --time=24:00:00
-#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=6:00:00
+#SBATCH --cpus-per-task=2
 #SBATCH --output="ttseq_mapping_%j.out"
 #SBATCH --mail-type=ALL
 
@@ -57,61 +57,11 @@ mkdir -p QC/fastp
 mkdir -p QC/fragmentSize
 
 mv *.fastq.gz fastq/raw
-##############
-## Initial QC
-##############
-echo "QC and adapter trimming"
-for samp in ${sampFiles[*]}
-	do
-	echo "trimming ${samp} by fastp"
-	fastp -i fastq/raw/"${samp}"_1.fastq.gz -I fastq/raw/"${samp}"_2.fastq.gz \
-		-w ${threads} -c \
-		--overlap_len_require 15 -o fastq/trim/"${samp}"_1_trim.fastq.gz \
-		-O fastq/trim/"${samp}"_2_trim.fastq.gz
-	rm ./*fastp.json
-	mv fastp.html QC/fastp/"${samp}".fastp.html
-    echo ""
-done
-
-##############
-## Aligning ##
-##############
-
-for samp in ${sampFiles[*]}
-do
-	if [[ ! -s bam/align/${samp}.sorted.bam ]]
-		then
-		echo "mapping ${samp}"
-		STAR \
-			--runThreadN ${threads} --runMode alignReads --genomeDir ${ref_hg38} \
-			--genomeFastaFiles /scratch/mford5/references/sacCer3/sacCer3spike.fa --readFilesIn fastq/trim/"${samp}"_1_trim.fastq.gz fastq/trim/"${samp}"_2_trim.fastq.gz \
-			--readFilesCommand zcat --quantMode TranscriptomeSAM GeneCounts \
-			--outSAMunmapped None \
-			--outSAMtype BAM Unsorted --outTmpDir tmp/"${samp}" \
-			--outFileNamePrefix bam/"${samp}". \
-			--sjdbGTFfile ${ref_hg38}/hg38.ensGene.gtf --outSAMmultNmax 1
-		samtools sort --threads ${threads} -o bam/"${samp}".sorted.bam bam/"${samp}".Aligned.out.bam
-	fi
-done
-####################
-## Bam processing ##
-####################
-
-for samp in ${sampFiles[*]}
-do
-java -jar /software/picard/2.12.0/picard.jar MarkDuplicates INPUT=bam/"${samp}".sorted.bam \
-OUTPUT=bam/"${samp}".sorted.dupMark.bam \
-METRICS_FILE=bam/"${samp}".sorted.marked.metrics \
-REMOVE_DUPLICATES=false \
-ASSUME_SORTED=true MAX_RECORDS_IN_RAM=2000000 VALIDATION_STRINGENCY=LENIENT \
-TMP_DIR=tmp/"${samp}" READ_NAME_REGEX=null
-samtools index bam/"${samp}".sorted.dupMark.bam
-done
 
 ######################
 # get scaling factor #
 ######################
-module load r
+module load r/4.2.1/b1
 Rscript --vanilla ttseq3scale.R
 
 mapfile -t scaleFactors < factors_tab.txt
